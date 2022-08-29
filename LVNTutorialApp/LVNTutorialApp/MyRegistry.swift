4fd288c54a86038fb5b7773562d5139cd3a54c6a
Shadowfacts <me@shadowfacts.net>
2022-08-29 18:26:54 -0400

    Section 5: Add a Navigation Bar Button with a Custom Attribute

--- LVNTutorialApp/LVNTutorialApp/MyRegistry.swift
+++ LVNTutorialApp/LVNTutorialApp/MyRegistry.swift
@@ -12,12 +12,22 @@ struct MyRegistry: CustomRegistry {
     enum TagName: String {
         case catRating = "cat-rating"
     }
-    typealias AttributeName = EmptyRegistry.None
+    enum AttributeName: String, Equatable {
+        case navFavorite = "nav-favorite"
+    }
     
     static func lookup(_ name: TagName, element: Element, context: LiveContext<MyRegistry>) -> some View {
         switch name {
         case .catRating:
             CatRatingView(element: element, context: context)
+        }
+    }
+    
+    static func applyCustomAttribute(_ name: AttributeName, value: String, element: Element, context: LiveContext<MyRegistry>) -> some View {
+        switch name {
+        case .navFavorite:
+            context.buildElement(element)
+                .modifier(NavFavoriteModifier(value: value, context: context))
         }
     }
 }
---
+++ LVNTutorialApp/LVNTutorialApp/NavFavoriteModifier.swift
@@ -0,0 +1,29 @@
+//
+//  NavFavoriteModifier.swift
+//  LVNTutorialApp
+//
+//  Created by Shadowfacts on 8/29/22.
+//
+
+import SwiftUI
+import PhoenixLiveViewNative
+
+struct NavFavoriteModifier: ViewModifier {
+    let value: String
+    let context: LiveContext<MyRegistry>
+    
+    func body(content: Content) -> some View {
+        content
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        Task {
+                            try? await context.coordinator.pushEvent(type: "click", event: "toggle-favorite", value: [:])
+                        }
+                    } label: {
+                        Image(systemName: value == "true" ? "star.fill" : "star")
+                    }
+                }
+            }
+    }
+}
