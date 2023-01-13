205d3821d8a646bfd541679a122e2d49002b9f30
Shadowfacts <me@shadowfacts.net>
2023-01-13 11:54:13 -0500

    Section 5: Add a Navigation Bar Button with a Custom Modifier

--- lvn_tutorial_backend/lib/lvn_tutorial_web/live/cat_live.ex
+++ lvn_tutorial_backend/lib/lvn_tutorial_web/live/cat_live.ex
@@ -12,11 +12,21 @@ defmodule LvnTutorialWeb.CatLive do
   )
 
   def mount(%{"name" => name}, _session, socket) do
-    {:ok, assign(socket, name: name, score: FavoritesStore.get_score(name))}
+    {:ok,
+     assign(socket,
+       name: name,
+       favorite: Enum.member?(FavoritesStore.get_favorites(), name),
+       score: FavoritesStore.get_score(name)
+     )}
   end
 
   def handle_event("change-score", score, socket) do
     FavoritesStore.set_score(socket.assigns.name, score)
     {:noreply, assign(socket, score: score)}
+  end
+
+  def handle_event("toggle-favorite", _, socket) do
+    new = FavoritesStore.toggle_favorite(socket.assigns.name)
+    {:noreply, assign(socket, favorite: Enum.member?(new, socket.assigns.name))}
   end
 end
